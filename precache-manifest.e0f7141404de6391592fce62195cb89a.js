d3c5bc2a00992d35f4e080022950f44a54edb4a4
Mark Edward Ratio <markedwardratio@gmail.com>
2020-01-15 15:36:24 +0800

    Updates

---
+++ precache-manifest.e0f7141404de6391592fce62195cb89a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163b9fd2b425e395083cff142eaf7ab5",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "65bd9ed4d51886364df0",
+    "url": "/calculator/static/css/main.cb846e1e.chunk.css"
+  },
+  {
+    "revision": "3cb654476ad4df544c75",
+    "url": "/calculator/static/js/2.42576b40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.42576b40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65bd9ed4d51886364df0",
+    "url": "/calculator/static/js/main.c0c7c22a.chunk.js"
+  },
+  {
+    "revision": "cda3e23f75020296141f",
+    "url": "/calculator/static/js/runtime-main.419307e5.js"
+  }
+]);
