369ad43c155b6f3b804f0856345e647cbc17f724
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-08-07 07:53:21 +0800

    Add operate()

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operandA, operandB, operator) {
+  switch (operator) {
+    case '+':
+      return add(operandA, operandB);
+    case '-':
+      return subtract(operandA, operandB);
+    case '*':
+      return multiply(operandA, operandB);
+    case '/':
+      return divide(operandA, operandB);
+  }
+}
