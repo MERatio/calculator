74c0fedb3c1af9c036dfd05cb5edc6511e259db5
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-08-07 11:33:21 +0800

    Move calculator stuff in an object

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 'use strict';
 
-let operandA = null;
-let operandB = null;
-let operator = null;
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function operate(operandA, operandB, operator) {
-  switch (operator) {
-    case '+':
-      return add(operandA, operandB);
-    case '-':
-      return subtract(operandA, operandB);
-    case '*':
-      return multiply(operandA, operandB);
-    case '/':
-      return divide(operandA, operandB);
-  }
-}
+const calculator = {
+  operandA: null,
+  operandB: null,
+  operator: null,
+  add(operandA, operandB) {
+    return operandA + operandB;
+  },
+  subtract(operandA, operandB) {
+    return operandA - operandB;
+  },
+  multiply(operandA, operandB) {
+    return operandA * operandB;
+  },
+  divide(operandA, operandB) {
+    return operandA / operandB;
+  },
+  operate(operandA, operandB, operator) {
+    switch (operator) {
+      case '+':
+        return this.add(operandA, operandB);
+      case '-':
+        return this.subtract(operandA, operandB);
+      case '*':
+        return this.multiply(operandA, operandB);
+      case '/':
+        return this.divide(operandA, operandB);
+    }
+  },
+};
