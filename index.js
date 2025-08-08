f6d8ff01e632aafc5b22ec58fad8dd43f14b9254
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-08-08 10:27:29 +0800

    Add display, numbers, +,-,*,/,= functionality, and refactor

--- index.js
+++ index.js
@@ -1,31 +1,88 @@
 'use strict';
 
-const calculator = {
-  operandA: null,
-  operandB: null,
-  operator: null,
-  add(operandA, operandB) {
-    return operandA + operandB;
-  },
-  subtract(operandA, operandB) {
-    return operandA - operandB;
-  },
-  multiply(operandA, operandB) {
-    return operandA * operandB;
-  },
-  divide(operandA, operandB) {
-    return operandA / operandB;
-  },
-  operate(operandA, operandB, operator) {
-    switch (operator) {
-      case '+':
-        return this.add(operandA, operandB);
-      case '-':
-        return this.subtract(operandA, operandB);
-      case '*':
-        return this.multiply(operandA, operandB);
-      case '/':
-        return this.divide(operandA, operandB);
+const dom = {
+  numbersBtn: document.querySelectorAll('[data-number]'),
+  operatorsBtn: document.querySelectorAll('[data-operator]'),
+  equalsBtn: document.querySelector('#equalsBtn'),
+  determineDisplayOutput() {
+    const calculatorDisplay = document.querySelector('#calculatorDisplay');
+    calculatorDisplay.textContent = calculator.operandA;
+    if (calculator.operator) {
+      calculatorDisplay.textContent += ' ' + calculator.operator;
+      if (calculator.operandB) {
+        calculatorDisplay.textContent += ' ' + calculator.operandB;
+      }
     }
   },
 };
+
+const calculator = {
+  operandA: '0',
+  operandB: '',
+  operator: null,
+  operate() {
+    const a = parseFloat(this.operandA);
+    const b = parseFloat(this.operandB);
+    switch (this.operator) {
+      case '+':
+        return a + b;
+      case '-':
+        return a - b;
+      case '*':
+        return a * b;
+      case '/':
+        return a / b;
+    }
+  },
+  handleNumbersBtnClick(e) {
+    const numberStr = e.currentTarget.dataset.number;
+    if (this.operator === null) {
+      if (this.operandA === '0') {
+        this.operandA = numberStr;
+      } else {
+        this.operandA += numberStr;
+      }
+    } else {
+      this.operandB += numberStr;
+    }
+    dom.determineDisplayOutput();
+  },
+  handleOperatorClick(e) {
+    const operator = e.currentTarget.dataset.operator;
+    if (this.operator === null) {
+      this.operator = operator;
+    } else {
+      if (this.operandB === '') {
+        return;
+      }
+    }
+    dom.determineDisplayOutput();
+  },
+  handleEqualsClick() {
+    if (this.operator && this.operandB) {
+      this.operandA = this.operate().toString();
+      this.operandB = '';
+      this.operator = null;
+      dom.determineDisplayOutput();
+    }
+  },
+};
+
+function addEventListeners() {
+  for (const numberBtn of dom.numbersBtn) {
+    numberBtn.addEventListener('click', (e) =>
+      calculator.handleNumbersBtnClick(e)
+    );
+  }
+
+  for (const operatorBtn of dom.operatorsBtn) {
+    operatorBtn.addEventListener('click', (e) =>
+      calculator.handleOperatorClick(e)
+    );
+  }
+
+  dom.equalsBtn.addEventListener('click', () => calculator.handleEqualsClick());
+}
+
+dom.determineDisplayOutput();
+addEventListeners();
